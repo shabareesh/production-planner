f18ad41dcbea883ec4bd863608f32ba009984f10
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-15 18:10:33 +0530

    Create PageHeader component and Integrate it with App Component

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+
+const PageHeader = () => (
+    <div className="page-header">
+        <AppBar position="fixed">
+            <Typography variant="h6" noWrap>
+                Production Planner
+            </Typography>
+        </AppBar>
+    </div>
+);
+
+export default PageHeader;

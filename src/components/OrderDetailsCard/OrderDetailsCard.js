ebe58cb6363506f1b637c35c12c4702f04f6b5a1
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 18:12:56 +0530

    Add Styles to OrderDetailsCard component

--- src/components/OrderDetailsCard/OrderDetailsCard.js
+++ src/components/OrderDetailsCard/OrderDetailsCard.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import CheckIcon from '@material-ui/icons/Check';
+import BlockIcon from '@material-ui/icons/Block';
 import ProductionTimeBar from "../ProductionTimeBar/ProductionTimeBar";
 
 import './orderDetailsCard.scss';
 
-const OrderDetailsCard = ({ orderNumber, name, quantity, hasMaterial, totalTime, runningTime, preparedTime }) => (
-    <div className="order-details-card">
-        <span className="order-details-card--orderNumber">Order No. {orderNumber}</span>
-        <span className="order-details-card--name">{name}</span>
-        <ProductionTimeBar preparedTime={preparedTime} runningTime={runningTime} totalTime={totalTime} />
-        <span className="order-details-card--material">Material {hasMaterial}</span>
-        <span className="order-details-card--quantity">Quantity {quantity}</span>
-    </div>
+const OrderDetailsCard = ({
+  orderNumber,
+  name,
+  quantity,
+  hasMaterial,
+  totalTime,
+  runningTime,
+  preparedTime,
+  shift,
+}) => (
+  <div className="order-details-card" style={{width: `${200 * shift}px`}}>
+    <span className="order-details-card--orderNumber">Order No. {orderNumber}</span>
+    <span className="order-details-card--name">{name}</span>
+    <ProductionTimeBar preparedTime={preparedTime} runningTime={runningTime} totalTime={totalTime} />
+    <span className="order-details-card--material">
+      Material
+      {hasMaterial ? <CheckIcon style={{ color: '#4BB04F' }} /> : <BlockIcon color="secondary" />}
+    </span>
+    <span className="order-details-card--quantity">Quantity {quantity}</span>
+  </div>
 );
 
 export default OrderDetailsCard;

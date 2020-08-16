dade14d95ab811336725998d49297b1584fb3530
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 18:03:55 +0530

    Add mock data into data.js file

---
+++ src/common/data.js
@@ -0,0 +1,97 @@
+export const data = [
+	{
+		workStation: "Drehan",
+		machines: [
+			{
+				name: "Drehmaschine 312",
+				status: "active",
+				orders: [
+					{
+						orderNumber: "2202/3284",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 400,
+						totalTime: "12:00",
+						preparedTime: "2:00",
+						runningTime: "2:00",
+						date: "13-07-2020",
+						startedTime: "00:00"
+					},
+					{
+						orderNumber: "2202/3621",
+						name: "Propeller",
+						hasMaterial: false,
+						quantity: 200,
+						totalTime: "8:00",
+						preparedTime: "1:00",
+						runningTime: "1:00",
+						date: "14-07-2020",
+						startedTime: "02:00"
+					},
+					{
+						orderNumber: "2202/3131",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 40,
+						totalTime: "11:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "15-07-2020",
+						startedTime: "04:00"
+					}
+				]
+			},
+			{
+				name: "Drehmaschine 212",
+				status: "active",
+				orders: [
+					{
+						orderNumber: "2202/3749",
+						name: "Pumpe",
+						hasMaterial: true,
+						quantity: 1340,
+						totalTime: "24:00",
+						preparedTime: "2:00",
+						runningTime: "17:00",
+						date: "13-07-2020",
+						startedTime: "00:00"
+					},
+					{
+						orderNumber: "2202/2123",
+						name: "Pumpe",
+						hasMaterial: true,
+						quantity: 1100,
+						totalTime: "24:00",
+						preparedTime: "2:00",
+						runningTime: "10:00",
+						date: "14-07-2020",
+						startedTime: "02:00"
+					},
+					{
+						orderNumber: "2202/9911",
+						name: "Pumpe",
+						hasMaterial: false,
+						quantity: 1240,
+						totalTime: "29:00",
+						preparedTime: "2:00",
+						runningTime: "10:00",
+						date: "15-07-2020",
+						startedTime: "05:00"
+					}
+				]
+			}
+		]
+	},
+	{
+		workStation: "Work Station 1",
+		machines: []
+	},
+	{
+		workStation: "Work Station 2",
+		machines: []
+	},
+	{
+		workStation: "Work Station 3",
+		machines: []
+	}
+];

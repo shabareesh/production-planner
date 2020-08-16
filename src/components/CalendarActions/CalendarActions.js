cc7b6d3dfdcbc7527cdf973477156d5707151255
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 18:10:50 +0530

    Optimize CalendarActions component

--- src/components/CalendarActions/CalendarActions.js
+++ src/components/CalendarActions/CalendarActions.js
@@ -1,56 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dropdown from "../Dropdown/Dropdown";
 import DatePicker from "../DatePicker/DatePicker";
 import WeekPicker from "../WeekPicker/WeekPicker";
 
-const CalendarActions = () => {
-    const workStationOptions = [
-        { label: 'Drehen', value: 'Drehen' },
-        { label: 'Work Station 1', value: 'Work Station 1' },
-        { label: 'Work Station 2', value: 'Work Station 2' },
-        { label: 'Work Station 3', value: 'Work Station 3' },
-    ];
+import './calendarActions.scss';
 
-    const calendarViewOptions = [
-        { label: 'Weekly', value: 'Weekly' },
-        { label: 'Daily', value: 'Daily' },
-    ]
-
-    const [workStation, setWorkStation] = useState(workStationOptions[0].value);
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [selectedCalendarView, setSelectedCalendarView] = useState(calendarViewOptions[0].value);
-
-    const handleWorkStationChange = (e) => {
-        setWorkStation(e.target.value);
-    };
-
-    const handleCalendarViewChange = (e) => {
-        setSelectedCalendarView(e.target.value);
-    };
-
-    return (
-        <div className="calendar-actions">
-            <div className="calendar-actions--workstation">
-                <Dropdown
-                    options={workStationOptions}
-                    onChange={handleWorkStationChange}
-                    selectedValue={workStation}
-                    label="Work Station"
-                />
-            </div>
-            <div className="calendar-actions--date">
-                {selectedCalendarView === 'Daily' ?
-                    <DatePicker selectedDate={selectedDate} onChange={setSelectedDate} /> :
-                    <WeekPicker selectedDate={selectedDate} onChange={setSelectedDate} />
-                }
-                <Dropdown
-                    options={calendarViewOptions}
-                    onChange={handleCalendarViewChange}
-                    selectedValue={selectedCalendarView}
-                />
-            </div>
+const CalendarActions = ({
+     workStationOptions,
+     calendarViewOptions,
+     handleDateChange,
+     handleWorkStationChange,
+     handleCalendarViewChange,
+     workStation,
+     selectedDate,
+     selectedCalendarView,
+}) => (
+    <div className="calendar-actions">
+        <div className="calendar-actions--workstation">
+            <Dropdown
+                options={workStationOptions}
+                onChange={handleWorkStationChange}
+                selectedValue={workStation}
+                label="Work Station"
+            />
         </div>
-    );
-};
+        <div className="calendar-actions--date">
+            {selectedCalendarView === 'Daily' ?
+                <DatePicker selectedDate={selectedDate} onChange={handleDateChange}/> :
+                <WeekPicker selectedDate={selectedDate} onChange={handleDateChange}/>
+            }
+            <Dropdown
+                options={calendarViewOptions}
+                onChange={handleCalendarViewChange}
+                selectedValue={selectedCalendarView}
+            />
+        </div>
+    </div>
+);
 
 export default CalendarActions;

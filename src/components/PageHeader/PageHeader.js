93d4ab128140bb5c9c445670abb378da89dbf90e
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 18:00:15 +0530

    Fix PageHeader alignment

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const PageHeader = () => (
-    <div className="page-header">
-        <AppBar position="static">
-            <Typography variant="h6" noWrap>
-                Production Planner
-            </Typography>
-        </AppBar>
-    </div>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" noWrap>
+        Production Planner
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
 
 export default PageHeader;

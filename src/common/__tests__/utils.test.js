c3970eca80abfa70a309e938ebf12969f77191f9
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 18:01:02 +0530

    Add getDate, isDateEqual, isDateInRange and getWeekName utils

--- src/common/__tests__/utils.test.js
+++ src/common/__tests__/utils.test.js
@@ -1,9 +1,30 @@
-import {calculatePercent} from "../utils";
+import {calculatePercent, isDateEqual, isDateInRange, getDate, getWeekName} from "../utils";
 
 describe('Utils', () => {
     test('should calculate percentage', () => {
         expect(calculatePercent(12, 8)).toEqual("66.67");
         expect(calculatePercent(12, 2)).toEqual("16.67");
         expect(calculatePercent(24, 17)).toEqual("70.83");
+    });
+
+    test('should check date equality', () => {
+        expect(isDateEqual('13-07-2020', new Date(2020, 6, 13))).toBeTruthy();
+        expect(isDateEqual('13-07-2020', '13-07-2020')).toBeTruthy();
+        expect(isDateEqual('13-07-2020', '12-07-2020')).toBeFalsy();
+    });
+
+    test('should check date in range with the selected date', () => {
+        expect(isDateInRange('13-07-2020', '15-07-2020')).toBeTruthy();
+        expect(isDateInRange('13-07-2020', '25-07-2020')).toBeFalsy();
+        expect(isDateInRange('13-07-2020', '13-07-2020')).toBeTruthy();
+    });
+
+    test('should convert string format date to actual date', () => {
+        expect(getDate('13-07-2020')).toEqual(new Date(2020, 6, 13));
+        expect(getDate('15-07-2020')).toEqual(new Date(2020, 6, 15));
+    });
+
+    test('should get the week name from the given date', () => {
+       expect(getWeekName('13-07-2020')).toEqual('Monday');
     });
 });
--- src/common/utils.js
+++ src/common/utils.js
@@ -1,1 +1,36 @@
+import startOfWeek from "date-fns/startOfWeek";
+import isEqual from "date-fns/isEqual";
+
 export const calculatePercent = (total, num) => ((num / total) * 100).toFixed(2);
+
+export const timeToFloat = (time) => {
+  const hoursMinutes = time.split(/[.:]/);
+  const hours = parseInt(hoursMinutes[0], 10);
+  const minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+
+  return hours + minutes / 60;
+};
+
+export const getDate = (date) => {
+  const splitDate = date.split('-');
+  return new Date(splitDate[2], +splitDate[1] - 1, splitDate[0]);
+};
+
+export const isDateEqual = (date, selectedDate) => {
+  const dateLeft = date instanceof Date ? date : getDate(date);
+  const dateRight = selectedDate instanceof Date ? selectedDate : getDate(selectedDate);
+  return isEqual(dateLeft, dateRight);
+};
+
+export const isDateInRange = (date, selectedDate) => {
+  const startDay = date instanceof Date ? startOfWeek(date) : startOfWeek(getDate(date));
+  const selectedStartDay = selectedDate instanceof Date ? startOfWeek(selectedDate) : startOfWeek(getDate(selectedDate));
+
+  return isDateEqual(startDay, selectedStartDay);
+};
+
+export const getWeekName = (date) => {
+  const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const weekIndex = getDate(date).getDay();
+  return week[weekIndex];
+};

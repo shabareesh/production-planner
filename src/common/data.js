7582977e3b8146ca10035371cd574383d1fc2717
Shabareesh JL <shabareeshjl@sjl-a01.vmware.com>
2020-08-16 21:10:55 +0530

    Update mock data with more machines and orders

--- src/common/data.js
+++ src/common/data.js
@@ -79,6 +79,95 @@ export const data = [
 						startedTime: "05:00"
 					}
 				]
+			},
+			{
+				name: "Drehmaschine 112",
+				status: "running",
+				orders: [
+					{
+						orderNumber: "2202/3712",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 120,
+						totalTime: "7:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "13-07-2020",
+						startedTime: "00:00"
+					},
+					{
+						orderNumber: "2202/3733",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 150,
+						totalTime: "7:00",
+						preparedTime: "1:00",
+						runningTime: "1:00",
+						date: "13-07-2020",
+						startedTime: "00:00"
+					},
+					{
+						orderNumber: "2202/2121",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 210,
+						totalTime: "11:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "14-07-2020",
+						startedTime: "03:00"
+					},
+					{
+						orderNumber: "2202/9826",
+						name: "Propeller",
+						hasMaterial: false,
+						quantity: 90,
+						totalTime: "11:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "14-07-2020",
+						startedTime: "02:00"
+					},
+					{
+						orderNumber: "2202/3612",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 400,
+						totalTime: "11:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "15-07-2020",
+						startedTime: "01:00"
+					},
+					{
+						orderNumber: "2202/9183",
+						name: "Propeller",
+						hasMaterial: true,
+						quantity: 400,
+						totalTime: "11:00",
+						preparedTime: "2:00",
+						runningTime: "1:00",
+						date: "15-07-2020",
+						startedTime: "01:00"
+					}
+				]
+			},
+			{
+				name: "Drehmaschine 782",
+				status: "stopped",
+				orders: [
+					{
+						orderNumber: "2202/3684",
+						name: "Pumpe",
+						hasMaterial: true,
+						quantity: 1440,
+						totalTime: "24:00",
+						preparedTime: "3:00",
+						runningTime: "4:00",
+						date: "13-07-2020",
+						startedTime: "03:00"
+					}
+				]
 			}
 		]
 	},
